669706b32b09f58f06feaa3a50cafe66e317a45c
Ztcz <96683169+ZtczGrowtopia@users.noreply.github.com>
2022-12-18 15:56:25 +0300

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+title Building...
+cls
+pyarmor pack -e" --onefile --noconsole --icon ICON.ico" grimxd.py
+cls
+echo Python File Obfuscated And Builded. Check Dist Folder If Theres No .EXE Try Downloading Python 3.10.8 // --ONEFILE --NOCONSOLE --ICON
+pause
---
+++ setup.bat
@@ -0,0 +1,8 @@
+@echo off
+title Setting Everything Up...
+cls
+pip install -r requirements.txt
+cls
+title Everything Done!
+echo Don't Forget To Use Python 3.10.8 Or It Won't Work! Required Modules Been Installed Now You Can Run Build.bat!
+pause
